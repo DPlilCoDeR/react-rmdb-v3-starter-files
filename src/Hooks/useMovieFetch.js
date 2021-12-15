aa484d529a1493ae01c491137a7dd731bc739b3a
Jason Nicolas Carvajal Perez <doble_papi@MacBook-Pro.local>
2021-12-15 15:39:28 +0100

    Session Storage - Movie

--- src/Hooks/useMovieFetch.js
+++ src/Hooks/useMovieFetch.js
@@ -3,6 +3,9 @@ import { useState, useEffect } from "react";
 //API
 import api from "../API";
 
+//Helper session storage
+import { isPersistedState } from '../helpers'
+
 export const useMovieFetch = movieId => {
     const [ state, setState] = useState({});
     const [ loading, setLoading] = useState(true);
@@ -26,10 +29,22 @@ export const useMovieFetch = movieId => {
             } catch (error) {
                 setError(true);
             }
+        };
+
+        const sessionState = isPersistedState(movieId);
+
+        if (sessionState) {
+            setState(sessionState);
+            setLoading(false);
+            return
         }
 
         fetchMovieData()
     },[movieId]);
+
+    useEffect(() => {
+        sessionStorage.setItem(movieId, JSON.stringify(state))
+    }, [movieId, state])
     
     return {state, loading, error};
 };
